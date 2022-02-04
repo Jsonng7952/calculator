7589b8aadad53386ff8a02726731f8071b1746b2
Jason Ng <jason.ng7952@gmail.com>
2022-02-04 18:20:13 -0500

    Updated

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ function multiply(n1, n2){
 }
 
 function divide(n1, n2){
+    if(n2 === 0){
+        return 'ERROR';
+    }
     return n1 / n2;
 }
 
@@ -31,35 +34,92 @@ function operate(operator, n1, n2){
 }
 
 function numberPressed(number){
-    currNumber += number.id;
-    display.innerHTML = currNumber;
+    // Number display
+    if(currNumber.length < 10){
+        // '0.[number]'
+        if(currNumber.length < 1 && number.id === '.'){
+            currNumber += `0${number.id}`;
+            display.innerHTML = currNumber;  
+            decimal.disabled = true;
+        }
+        // '[number].'
+        else if(currNumber.length > 0 && number.id === '.'){
+            currNumber += number.id;
+            display.innerHTML = currNumber;  
+            decimal.disabled = true;       
+        }
+        else {
+            currNumber += number.id;
+            display.innerHTML = currNumber;  
+        }        
+    }
+    // Save the current operator and number
     if(pair.length === 1 && currOperator !== "="){
         pair.push(currOperator);
+        decimal.disabled = false;
     }
+    // Operation reset if the equal is pressed before a full pair is established.
     else if(pair.length === 1 && currOperator === "="){
         pair = [];
+        decimal.disabled = false;
     }
 }
 
 function operatorPressed(operator){
     currOperator = operator.id;
+    
+    // if operator is pressed and a number exist, store the current number.
     if(pair.length < 1 && currNumber.length !== 0){
-        pair.push(parseInt(currNumber));
+        pair.push(parseFloat(currNumber));
         currNumber = "";
+        decimal.disabled = false;
     }
-    else if(pair.length === 2){
-        pair.push(parseInt(currNumber));
+    else if(pair.length === 2){                             
+        pair.push(parseFloat(currNumber));
         currNumber = "";
-        currResult = operate(pair[1], pair[0], pair[2]);
+        currResult = operate(pair[1], pair[0], pair[2]).toString().substr(0, 10);    
         display.innerHTML = currResult;
         pair = [];
         pair.push(currResult);
+        decimal.disabled = false;
     }
 }
 
+// Clear the current display, reset all fields.
 function clearDisplay(){
-    display.innerHTML = "&nbsp";
+    display.innerHTML = "0";
     currNumber = "", currOperator = "", currResult = "", pair = [];
+    decimal.disabled = false;
+}
+
+function deleteDisplay(){
+    if(currResult === 'ERROR'){
+        display.innerHTML = "0";        
+    }
+    if(currNumber.length > 0){
+        if(currNumber.slice(-1) === '.'){
+            currNumber = currNumber.substring(0, currNumber.length - 1);
+            decimal.disabled = false;
+            // If the currentNumber is 0., make sure to reset it to "" after backspace.
+            if(currNumber.length === 1 && currNumber === "0"){
+                currNumber = "";
+                display.innerHTML = "0";
+            }
+            else{
+                display.innerHTML = currNumber;
+            }
+        }
+        else{
+            currNumber = currNumber.substring(0, currNumber.length - 1);
+            if(currNumber.length === 0){
+                display.innerHTML = "0";
+            }
+            else{
+                display.innerHTML = currNumber;
+            }
+        }
+       
+    }
 }
 
 let currNumber = "";
@@ -69,15 +129,25 @@ let pair = [];
 
 const display = document.querySelector('.display');
 
-const numbers = document.querySelectorAll('.btn-container .numbers button');
+const numbers = document.querySelectorAll('.btn-container .numbers .number');
 numbers.forEach(number => {
     number.addEventListener('click', () => numberPressed(number))
 });
 
-const operators = document.querySelectorAll('.btn-container .operators button');
+const decimal = document.querySelector('.float');
+decimal.addEventListener('click', () => numberPressed(decimal));
+
+const operators = document.querySelectorAll('.btn-container .operators .operator');
 operators.forEach(operator => {
     operator.addEventListener('click', () => operatorPressed(operator))
 });
 
-const clear = document.querySelector('.delete #clear');
+const equal = document.querySelector('.btn-container .numbers .operator');
+equal.addEventListener('click', () => operatorPressed(equal));
+
+const clear = document.querySelector('.delete-container #clear');
 clear.addEventListener('click', clearDisplay);
+
+const backspace = document.querySelector('.delete-container #backspace');
+backspace.addEventListener('click', deleteDisplay);
+
